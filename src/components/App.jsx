6b30e78b46f8ed4f707054dc94d5c366105cf6c1
Viktor-117 <viktor.hushchin@gmail.com>
2022-11-07 13:50:51 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Route, Routes } from 'react-router-dom';
+import { NavMenu } from './App.styled';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,12 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 };
---
+++ src/components/App.styled.js
@@ -0,0 +1,5 @@
+import { styled } from 'styled-components';
+
+export const NavMenu = styled.nav`
+  padding: 10px, 20px;
+`;
